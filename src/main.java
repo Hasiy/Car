67a4c487a406cbae9c61b1ab2f4e28728e54c5b1
Hasiy <15261985733@163.com>
2017-06-25 00:49:51 +0800

    begin
    begin

---
+++ src/main.java
@@ -0,0 +1,24 @@
+/**
+ * Created by zhuxiaoyao on 2017/6/24.
+ */
+
+import java.sql.*;
+public class main {
+    public static void main(String [] args){
+        String driverName="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        String dbURL="jdbc:sqlserver://localhost:1433;DatabaseName=Text";
+        String userName="sa";
+        String userPwd="512512";
+        Connection ConnectiondbConn;
+        try{
+            Class.forName(driverName);
+            ConnectiondbConn= DriverManager.getConnection(dbURL,userName,userPwd);
+            ConnectiondbConn.setAutoCommit(true);
+            System.out.println("连接数据库成功");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.print("连接失败");
+        }
+    }
+}
